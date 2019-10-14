bed345a243a6273ba5e3e579f1940926c799baf2
8kball <diegomessenger@gmail.com>
2019-10-14 10:43:39 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/example/cursomc/services/CategoriaService.java
+++ src/main/java/com/example/cursomc/services/CategoriaService.java
@@ -58,6 +58,6 @@ public class CategoriaService {
 	}
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
-		return new Categoria(objDto.getId(), objDto.getNome());
+		return new Categoria(objDto.getId(), objDto.getNome()); 
 	}
 }
