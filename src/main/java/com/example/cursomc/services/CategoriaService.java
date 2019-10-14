f30967dc3807456683a72473970e9a13ea666967
8kball <diegomessenger@gmail.com>
2019-10-13 21:41:22 -0300

    Listando todas categorias com DTO

--- src/main/java/com/example/cursomc/services/CategoriaService.java
+++ src/main/java/com/example/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.example.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +40,11 @@ public class CategoriaService {
 			repo.deleteById(id);
 		}
 		catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível uma categoria que possui produtos!");
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
